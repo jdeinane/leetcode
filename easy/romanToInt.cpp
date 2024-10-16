01b80d1b868d7dccd44205109fc22f54ebd70efa
Jennifer Ubaldo <jenniferdianeu@gmail.com>
2024-10-17 01:34:59 +0200

    romanToInt

--- easy/romanToInt.cpp
+++ easy/romanToInt.cpp
@@ -2,12 +2,28 @@
 #include <string>
 #include <iostream>
 #include <algorithm>
+#include <map>
 #include <unordered_map>
+#include <fstream>
 
 int romanToInt(std::string s) {
-	std::unordered_map<std::string, int> roman = {{"I", 3}, {"V", 5}, {"X", 10},
-								{"L", 50}, {"C", 100}, {"D", 500}, {"M", 1000}};
+    std::unordered_map<char, int> map = {{'I', 1}, {'V', 5}, {'X', 10}, {'L', 50}, {'C', 100}, {'D', 500}, {'M', 1000}};
 
-	for (int i = 0; i < s.size(); ++i) {
+	int result = map[s[s.length() - 1]];
+	
+	for (int i = s.length() - 2; i >= 0; i--) {
+		if (map[s[i]] < map[s[i + 1]])
+			result -= map[s[i]];
+		else
+			result += map[s[i]];
 	}
+	return result;
+}
+
+int main() {
+	std::string s = "MCMXCIV";
+	int result = romanToInt(s);
+
+	std::cout << result << std::endl;
+	return 0;
 }
